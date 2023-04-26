ea45dfa9597ebd757fb1955ac835f9e91e5218ec
Alex <alexander.krivorotko@gmail.com>
2023-04-26 13:05:10 +0700

    cleanup

--- mock-sfdc-server.js
+++ mock-sfdc-server.js
@@ -8,12 +8,6 @@ app.get('/api/sfdc/accounts', (req, res) => {
     res.json(JSON.parse(accounts.toString()));
 });
 
-app.post('/api/sfdc/accounts', (req, res) => {
-    if (!req.body) return res.status(400).send({error: 'Body is undefined'});
-    accounts.push(req.body);
-    res.json(accounts.find(entry => entry.id === req.body.id));
-});
-
 app.get('/api/sfdc/contacts', (req, res) => {
     res.json(JSON.parse(contacts.toString()));
 });
